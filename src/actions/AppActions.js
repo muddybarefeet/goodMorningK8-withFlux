522176602777e003be13af978fefd32c8e9c70f9
Rohan Pethiyagoda <RP-3@users.noreply.github.com>
2015-09-17 10:51:45 -0700

    Update AppActions.js
    Fixed up init method to get weather

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -8,26 +8,31 @@ myDataRef.on('child_added', function(snapshot) {
   AppActions.recieveMessage(message);
 });
 
-
-var weather = request('http://api.openweathermap.org/data/2.5/weather?lat=37.7749295&lon=-122.4194155', function w(er, res) {
-  if(!er) {
-    var weatherObj = JSON.parse(res.body);
-    var id = weatherObj.weather[0].id;
-    var averageTempCelc = Math.round((weatherObj.main.temp)-273.15); //average temp converted from kelvin to celcius
-    //var averageTempFar = Math.round( ((((weatherObj.main.temp) - 273.15)* 1.8000)+32)  );
-    var exportObj = {
-      temp : averageTempCelc,
-      weatherId : id
-    };
-    console.log('weather running');
-    AppActions.newWeather(exportObj);
-  }
-});
-
+//Request is an Async function, and doesn't return anything. So there's no point assigning to an invokation of request =)
+//instead let's chuck it directly in your init function
 var AppActions = {
 
   init: function() {
-    return weather;
+    //so I'm saying request the weather, and then, take the err and data that I get back, and pass them to AppActions.recieveWeather
+    request('http://api.openweathermap.org/data/2.5/weather?lat=37.7749295&lon=-122.4194155', AppActions.recieveWeather);
+  },
+  
+  //recieveWeather does whatever you want with the err and response you get back =)
+  recieveWeather: function(err, res){
+    if(!err) {
+        var weatherObj = JSON.parse(res.body);
+        var id = weatherObj.weather[0].id;
+        var averageTempCelc = Math.round((weatherObj.main.temp)-273.15); //average temp converted from kelvin to celcius
+        //var averageTempFar = Math.round( ((((weatherObj.main.temp) - 273.15)* 1.8000)+32)  );
+        var exportObj = {
+          temp : averageTempCelc,
+          weatherId : id
+        };
+        console.log('weather running');
+        AppActions.newWeather(exportObj);
+      }else{
+        console.log('Failed to fetch weather: ', err)
+      }
   },
 
   exampleAction: function(text){ //sends object to dispatcher with details about it in an object
